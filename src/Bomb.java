a080dd6397596b31b7c29e66d29ec76d1870e1ee
nithintata <nithin.tata2000@gmail.com>
2020-04-23 15:29:57 +0530

    Initial commit

---
+++ src/Bomb.java
@@ -0,0 +1,25 @@
+
+/**
+ * Bomb class
+ */
+public class Bomb extends BaseObject {
+    public Bomb(double x, double y) {
+        super(x, y, 1);
+    }
+
+    /**
+     * Draw the object on the canvas.
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.setPoint(x, y, 'B');
+    }
+
+    /**
+     * Move the object down by one step.
+     */
+    @Override
+    public void move() {
+        y++;
+    }
+}
---
+++ src/Rocket.java
@@ -0,0 +1,26 @@
+
+/**
+ * Rocket class
+ */
+public class Rocket extends BaseObject {
+
+    public Rocket(double x, double y) {
+        super(x, y, 1);
+    }
+
+    /**
+     * This method draws the object on the canvas.
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.setPoint(x, y, 'R');
+    }
+
+    /**
+     * Move the object up by one step.
+     */
+    @Override
+    public void move() {
+        y--;
+    }
+}
